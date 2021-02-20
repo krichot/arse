3c15bb68f0df0ba23a0fd42da03919f5d8b10f48
Krzysztof Sprawnik <krichot@gmail.com>
2021-02-20 12:34:46 +0100

    working settings dump

--- docker/webapp/app.py
+++ docker/webapp/app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html", template_folder = "templates", config=configs)
 
-@app.route("/help")
+@app.route("/settings")
 def help():
-    return render_template("help.html", template_folder = "templates", appVersion="1.1")
+    return render_template("settings.html", template_folder = "templates", configs=configs)
 
 if __name__ == "__main__":
+
+    # load configuration
+    # Read configuration JSON
+    with open('../config.json', 'r') as configFile:
+        data = configFile.read()
+    # Parse JSON file
+    configs = json.loads(data)
+
+    # run flask
     app.run(host='0.0.0.0', debug=True)
